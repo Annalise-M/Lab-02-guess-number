d9425e277c7beea2af55e655cffaa53e0a9ea833
Annalise Murphy <nali247@gmail.com>
2020-07-07 18:34:41 -0700

    got page to display tries remaining and such

--- app.js
+++ app.js
@@ -1,36 +1,13 @@
-// import functions and grab DOM elements
-import compareNumbers from './compareNumbers.js';
+import { compareNumbers } from './compareNumbers.js';
 
 const myButton = document.getElementById('submit-button');
 
-const triesLeft = document.getElementById('number-of-tries-left');
-const winSpan = document.getElementById('wins-span');
-// const lossSpan = document.getElementById('losses-span');
 const correctNumber = Math.ceil(Math.random() * 20);
 
-
-let userGuesses = 0;
-let userTriesLeft = 4;
-
-
 myButton.addEventListener('click', () => {
+    const guess = document.getElementById('num-guess').value;
     
-    const userGuess = document.getElementById('user-guess').value;
-    const userNumber = Number(userGuesses);
-
-    // console.log(typeof userNumber);
-    // makes the userGuess into a Number
-
-    const result = compareNumbers(userGuess, correctNumber);
-    userTriesLeft--;
+    compareNumbers(guess, correctNumber);
     
-    if (result === -1) {
-        triesLeft.textContent = `${userNumber} Too Low!`;
-        userTriesLeft--;
-    } else if (result === 1) {
-        userTriesLeft.textContent = `${userNumber} Too High!`;
-    } else {
-        winSpan.textContent = `${userNumber} You Win!`;
-    }
 
 });
--- compareNumbers.js
+++ compareNumbers.js
@@ -1,11 +1,22 @@
-function compareNumbers(guess, correctNumber) {
-    if (guess === correctNumber) {
-        return 0;
-    } else if (guess < correctNumber) {
-        return -1; }
-    else if (guess > correctNumber) {
-        return 1;
-    }
-}
+// const numGuess = document.getElementById('num-guess');
+const results = document.getElementById('results');
+const tries = document.getElementById('tries-left');
 
-export default compareNumbers;
+// num of tries
+let triesRemaining = 4;
+
+
+export function compareNumbers(guess, correctNumber) {
+    
+    if (guess > correctNumber) {
+        results.textContent = `Too high! Try again!`;
+        triesRemaining--;
+    } else if (guess < correctNumber) {
+        triesRemaining--;
+        results.textContent = `Too low! ${triesRemaining} tries left!`;
+    } else {
+        results.textContent = `You got it! You win!`;
+    }
+    tries.textContent = triesRemaining;
+    // console.log(correctNumber, triesRemaining, guess);
+}
